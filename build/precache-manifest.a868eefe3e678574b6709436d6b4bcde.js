cf5fb513a403972edb44b13a55665a23ebf25006
Micheal A <aad3rinto@gmail.com>
2022-10-11 12:09:15 +0000

    add logic related to mouseover and mouseleave

---
+++ build/precache-manifest.a868eefe3e678574b6709436d6b4bcde.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6961ea1f2dd3c5a5e0f9667e3b9c2dc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e760bff85e1f943012ed",
+    "url": "/static/js/2.48dff714.chunk.js"
+  },
+  {
+    "revision": "c9691f751e6c9c6bd3f9",
+    "url": "/static/js/main.74545c2a.chunk.js"
+  },
+  {
+    "revision": "8700f4be8df1ed4c39d8",
+    "url": "/static/js/runtime-main.03199c58.js"
+  }
+]);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+
+  function handleMouseAction1(){
+    setHeadingText("Hovering")
+  };
+
+  function handleMouseAction2(){
+    setHeadingText("Leaving")
+  };
+
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onMouseOver={handleMouseAction1} onMouseLeave={handleMouseAction2}>Submit</button>
     </div>
   );
 }
